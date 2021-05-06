54d33b861c40b6e2cd848dbdcfb4e3651bdbf08e
61481 <lukenicholls510@gmail.com>
2021-05-06 10:52:29 +0930

    updates

---
+++ nexo_template.bat
@@ -0,0 +1,5 @@
+:start
+@echo off
+color 0f
+title script-hub
+echo select a number to open it's corisponding script
---
+++ script_hub.bat
@@ -0,0 +1,7 @@
+:start
+@echo off
+color 0f
+title script-hub
+echo select a number to open it's corisponding script
+
+echo 1(script_name

5e42ab2680c74039ef1ac8a84debba42a277f359
61481 <lukenicholls510@gmail.com>
2021-05-06 12:42:01 +0930

    updates

--- matrix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color 0a
-set count=0
-:start
-set /A count=0+1
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-if %count% equ 100 exit /b

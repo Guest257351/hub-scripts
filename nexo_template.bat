f89572081aaf5648c3cf1995dad4d376acbc7f1f
61481 <lukenicholls510@gmail.com>
2021-05-07 09:51:18 +0930

    deleted

--- nexo_template.bat
+++
@@ -1,5 +0,0 @@
-:start
-@echo off
-color 0f
-title script-hub
-echo select a number to open it's corisponding script
--- script_hub.bat
+++
@@ -1,7 +0,0 @@
-:start
-@echo off
-color 0f
-title script-hub
-echo select a number to open it's corisponding script
-
-echo 1(script_name
